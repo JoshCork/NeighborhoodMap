98f993967c4b0f0d842ca25a3bd774d8d8dd1d71
Joshua Cork <joshua.cork@gmail.com>
2016-03-29 14:09:39 -0700

    going to have a single script to simplify things at first.... crawl, walk, run and all that

--- app/scripts/viewmodels/nmViewModel.js
+++
@@ -1,8 +0,0 @@
-// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
-function NmViewModel() {
-    this.firstName = ko.observable("Bert");
-    this.lastName = ko.observable("Bertington");
-}
-
-// Activates knockout.js
-ko.applyBindings(new NmViewModel());
